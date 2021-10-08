18f601e1ef33a89291764da0cb628f98a31f23f5
Kenneth Johnson <kd123johnson@gmail.com>
2021-10-08 09:33:46 -0600

    Initial Commit

---
+++ GradeCaculatorapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GradeCaculatorapp
+//
+//  Created by Kenneth Johnson on 10/8/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
