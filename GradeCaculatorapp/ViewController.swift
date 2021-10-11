ee947a766e2900e95f89718fe10f012205049791
Kenneth Johnson <kd123johnson@gmail.com>
2021-10-11 10:51:50 -0600

    Commit 2

--- GradeCaculatorapp/ViewController.swift
+++ GradeCaculatorapp/ViewController.swift
@@ -9,12 +9,31 @@ import UIKit
 
 class ViewController: UIViewController
 {
+    // Outlets
 
     @IBOutlet weak var gradeTotal: UILabel!
     
     @IBOutlet weak var yourCurrentGrade: UITextField!
     
     @IBOutlet weak var yourNeededGrade: UITextField!
+    
+    @IBOutlet weak var yourFinalExamWeight: UITextField!
+    
+    // Variables
+    var total = ""
+    
+    var total1 = ""
+    
+    var total2 = ""
+    
+    var myInt1 = 0
+    
+    var myInt2 = 0
+    
+    var myInt3 = 0
+    
+    var totalGrade = 0.0
+    
     
     override func viewDidLoad()
     {
@@ -23,8 +42,18 @@ class ViewController: UIViewController
     }
 
 
+    //Buttons
     @IBAction func caculateNumbers(_ sender: Any)
     {
+        total = yourCurrentGrade.text ?? ""
+        total1 = yourNeededGrade.text ?? ""
+        total2 = yourFinalExamWeight.text ?? ""
+        
+        myInt1 = Int(total) ?? 0
+        myInt2 = Int(total1) ?? 0
+        myInt3 = Int(total2) ?? 0
+        
+        //totalGrade = Double(("myInt1") + Double("myInt2") + Double("myInt3"))
         
     }
     
